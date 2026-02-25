32c9f7fd6566aab4a7dcb679cbf49b6369b5bc0b
naveenrajgit23 <naveenrajgit23@users.noreply.github.com>
2026-02-25 10:22:47 +0530

    Activation: Google Sheets Live Sync enabled

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
     // PASTE YOUR GOOGLE SCRIPT URL BELOW
     // Example: "https://script.google.com/macros/s/AKfyc.../exec"
-    SCRIPT_URL: ""
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxqreAemc4-fzhVdQfwfbsN_ontV1tGNXqswXx6HIGPSDK7SbtscoQyTDu-6oDHZdKb/exec"
 };
